9e3634d87543ff4c54a0d726617a1357bc1b02cb
hiscaler <hiscaler@gmail.com>
2022-04-23 11:29:05 +0800

    ...

--- product/product.go
+++ product/product.go
@@ -113,15 +113,107 @@ type ProductPicture struct {
 
 // ProductLogistic 物流关联
 type ProductLogistic struct {
+	// US
 	USCgTransportCosts float64 `json:"US_cg_transport_costs,omitempty"` // 美国默认头程成本(含税)
 	USCurrency         string  `json:"US_currency,omitempty"`           // 美国官方汇率code
 	USBgImportHsCode   string  `json:"US_bg_import_hs_code,omitempty"`  // 报关：美国HS Code（进口国）
 	USBgTaxRate        float64 `json:"US_bg_tax_rate,omitempty"`        // 报关：美国税率
 
-	JPCgTransportCosts float64 `json:"JP_cg_transport_costs,omitempty"` // 美国默认头程成本(含税)
-	JPCurrency         string  `json:"JP_currency,omitempty"`           // 美国官方汇率code
-	JPBgImportHsCode   string  `json:"JP_bg_import_hs_code,omitempty"`  // 报关：美国HS Code（进口国）
-	JPBgTaxRate        float64 `json:"JP_bg_tax_rate,omitempty"`        // 报关：美国税率
+	// CA
+	CACgTransportCosts float64 `json:"CA_cg_transport_costs,omitempty"`
+	CACurrency         string  `json:"CA_currency,omitempty"`
+	CABgImportHsCode   string  `json:"CA_bg_import_hs_code,omitempty"`
+	CABgTaxRate        float64 `json:"CA_bg_tax_rate,omitempty"`
+
+	// MX
+	MXCgTransportCosts float64 `json:"MX_cg_transport_costs,omitempty"`
+	MXCurrency         string  `json:"MX_currency,omitempty"`
+	MXBgImportHsCode   string  `json:"MX_bg_import_hs_code,omitempty"`
+	MXBgTaxRate        float64 `json:"MX_bg_tax_rate,omitempty"`
+
+	// JP
+	JPCgTransportCosts float64 `json:"JP_cg_transport_costs,omitempty"`
+	JPCurrency         string  `json:"JP_currency,omitempty"`
+	JPBgImportHsCode   string  `json:"JP_bg_import_hs_code,omitempty"`
+	JPBgTaxRate        float64 `json:"JP_bg_tax_rate,omitempty"`
+
+	// UK
+	UKCgTransportCosts float64 `json:"UK_cg_transport_costs,omitempty"`
+	UKCurrency         string  `json:"UK_currency,omitempty"`
+	UKBgImportHsCode   string  `json:"UK_bg_import_hs_code,omitempty"`
+	UKBgTaxRate        float64 `json:"UK_bg_tax_rate,omitempty"`
+
+	// DE
+	DECgTransportCosts float64 `json:"DE_cg_transport_costs,omitempty"`
+	DECurrency         string  `json:"DE_currency,omitempty"`
+	DEBgImportHsCode   string  `json:"DE_bg_import_hs_code,omitempty"`
+	DEBgTaxRate        float64 `json:"DE_bg_tax_rate,omitempty"`
+
+	// FR
+	FRCgTransportCosts float64 `json:"FR_cg_transport_costs,omitempty"`
+	FRCurrency         string  `json:"FR_currency,omitempty"`
+	FRBgImportHsCode   string  `json:"FR_bg_import_hs_code,omitempty"`
+	FRBgTaxRate        float64 `json:"FR_bg_tax_rate,omitempty"`
+
+	// IT
+	ITCgTransportCosts float64 `json:"IT_cg_transport_costs,omitempty"`
+	ITCurrency         string  `json:"IT_currency,omitempty"`
+	ITBgImportHsCode   string  `json:"IT_bg_import_hs_code,omitempty"`
+	ITBgTaxRate        float64 `json:"IT_bg_tax_rate,omitempty"`
+
+	// NL
+	NLCgTransportCosts float64 `json:"NL_cg_transport_costs,omitempty"`
+	NLCurrency         string  `json:"NL_currency,omitempty"`
+	NLBgImportHsCode   string  `json:"NL_bg_import_hs_code,omitempty"`
+	NLBgTaxRate        float64 `json:"NL_bg_tax_rate,omitempty"`
+
+	// ES
+	ESCgTransportCosts float64 `json:"ES_cg_transport_costs,omitempty"`
+	ESCurrency         string  `json:"ES_currency,omitempty"`
+	ESBgImportHsCode   string  `json:"ES_bg_import_hs_code,omitempty"`
+	ESBgTaxRate        float64 `json:"ES_bg_tax_rate,omitempty"`
+
+	// AU
+	AUCgTransportCosts float64 `json:"AU_cg_transport_costs,omitempty"`
+	AUCurrency         string  `json:"AU_currency,omitempty"`
+	AUBgImportHsCode   string  `json:"AU_bg_import_hs_code,omitempty"`
+	AUBgTaxRate        float64 `json:"AU_bg_tax_rate,omitempty"`
+
+	// SG
+	SGCgTransportCosts float64 `json:"SG_cg_transport_costs,omitempty"`
+	SGCurrency         string  `json:"SG_currency,omitempty"`
+	SGBgImportHsCode   string  `json:"SG_bg_import_hs_code,omitempty"`
+	SGBgTaxRate        float64 `json:"SG_bg_tax_rate,omitempty"`
+
+	// IN
+	INCgTransportCosts float64 `json:"IN_cg_transport_costs,omitempty"`
+	INCurrency         string  `json:"IN_currency,omitempty"`
+	INBgImportHsCode   string  `json:"IN_bg_import_hs_code,omitempty"`
+	INBgTaxRate        float64 `json:"IN_bg_tax_rate,omitempty"`
+
+	// AE
+	AECgTransportCosts float64 `json:"AE_cg_transport_costs,omitempty"`
+	AECurrency         string  `json:"AE_currency,omitempty"`
+	AEBgImportHsCode   string  `json:"AE_bg_import_hs_code,omitempty"`
+	AEBgTaxRate        float64 `json:"AE_bg_tax_rate,omitempty"`
+
+	// SA
+	SACgTransportCosts float64 `json:"SA_cg_transport_costs,omitempty"`
+	SACurrency         string  `json:"SA_currency,omitempty"`
+	SABgImportHsCode   string  `json:"SA_bg_import_hs_code,omitempty"`
+	SABgTaxRate        float64 `json:"SA_bg_tax_rate,omitempty"`
+
+	// BR
+	BRCgTransportCosts float64 `json:"BR_cg_transport_costs,omitempty"`
+	BRCurrency         string  `json:"BR_currency,omitempty"`
+	BRBgImportHsCode   string  `json:"BR_bg_import_hs_code,omitempty"`
+	BRBgTaxRate        float64 `json:"BR_bg_tax_rate,omitempty"`
+
+	// SE
+	SECgTransportCosts float64 `json:"SE_cg_transport_costs,omitempty"`
+	SECurrency         string  `json:"SE_currency,omitempty"`
+	SEBgImportHsCode   string  `json:"SE_bg_import_hs_code,omitempty"`
+	SEBgTaxRate        float64 `json:"SE_bg_tax_rate,omitempty"`
 }
 
 type ProductDetail struct {
@@ -188,6 +280,36 @@ func (s service) Product(id int) (item ProductDetail, err error) {
 				item = res.Data
 				if item.ID == 0 {
 					err = lingxing.ErrNotFound
+				} else {
+					fnValid := func(costs float64, hsCode string, taxRate float64) bool {
+						if costs == 0 && hsCode == "" && taxRate == 0 {
+							return false
+						}
+						return true
+					}
+					logistics := make([]ProductLogistic, 0)
+					for _, logistic := range item.ProductLogisticsRelation {
+						if fnValid(logistic.USCgTransportCosts, logistic.USBgImportHsCode, logistic.USBgTaxRate) ||
+							fnValid(logistic.CACgTransportCosts, logistic.CABgImportHsCode, logistic.CABgTaxRate) ||
+							fnValid(logistic.MXCgTransportCosts, logistic.MXBgImportHsCode, logistic.MXBgTaxRate) ||
+							fnValid(logistic.JPCgTransportCosts, logistic.JPBgImportHsCode, logistic.JPBgTaxRate) ||
+							fnValid(logistic.UKCgTransportCosts, logistic.UKBgImportHsCode, logistic.UKBgTaxRate) ||
+							fnValid(logistic.DECgTransportCosts, logistic.DEBgImportHsCode, logistic.DEBgTaxRate) ||
+							fnValid(logistic.FRCgTransportCosts, logistic.FRBgImportHsCode, logistic.FRBgTaxRate) ||
+							fnValid(logistic.ITCgTransportCosts, logistic.ITBgImportHsCode, logistic.ITBgTaxRate) ||
+							fnValid(logistic.NLCgTransportCosts, logistic.NLBgImportHsCode, logistic.NLBgTaxRate) ||
+							fnValid(logistic.ESCgTransportCosts, logistic.ESBgImportHsCode, logistic.ESBgTaxRate) ||
+							fnValid(logistic.AUCgTransportCosts, logistic.AUBgImportHsCode, logistic.AUBgTaxRate) ||
+							fnValid(logistic.SGCgTransportCosts, logistic.SGBgImportHsCode, logistic.SGBgTaxRate) ||
+							fnValid(logistic.INCgTransportCosts, logistic.INBgImportHsCode, logistic.INBgTaxRate) ||
+							fnValid(logistic.AECgTransportCosts, logistic.AEBgImportHsCode, logistic.AEBgTaxRate) ||
+							fnValid(logistic.SACgTransportCosts, logistic.SABgImportHsCode, logistic.SABgTaxRate) ||
+							fnValid(logistic.BRCgTransportCosts, logistic.BRBgImportHsCode, logistic.BRBgTaxRate) ||
+							fnValid(logistic.SECgTransportCosts, logistic.SEBgImportHsCode, logistic.SEBgTaxRate) {
+							logistics = append(logistics, logistic)
+						}
+					}
+					item.ProductLogisticsRelation = logistics
 				}
 			}
 		}
