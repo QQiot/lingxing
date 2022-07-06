a5719a0b072e40e1688d831deaf4b4c26e90ef73
hiscaler <hiscaler@gmail.com>
2022-07-06 10:36:29 +0800

    Bug: 修改 Rate.UpdateTime 类型为 string

--- basic.data.rate.go
+++ basic.data.rate.go
@@ -3,20 +3,19 @@ package lingxing
 import (
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	jsoniter "github.com/json-iterator/go"
-	"time"
 )
 
 // 费率
 // https://openapidoc.lingxing.com/#/docs/BasicData/Currency
 
 type Rate struct {
-	Date       string    `json:"date"`        // 汇率年月
-	Code       string    `json:"code"`        // 币种
-	Icon       string    `json:"icon"`        // 币种符号
-	Name       string    `json:"name"`        // 币种名
-	RateOrg    float64   `json:"rate_org"`    // 官方汇率（数据来源中国银行官方汇率）
-	MyRate     float64   `json:"my_rate"`     // 我的汇率（用户自定义汇率，系统首先使用该汇率数据）
-	UpdateTime time.Time `json:"update_time"` // 更新时间
+	Date       string  `json:"date"`        // 汇率年月
+	Code       string  `json:"code"`        // 币种
+	Icon       string  `json:"icon"`        // 币种符号
+	Name       string  `json:"name"`        // 币种名
+	RateOrg    float64 `json:"rate_org"`    // 官方汇率（数据来源中国银行官方汇率）
+	MyRate     float64 `json:"my_rate"`     // 我的汇率（用户自定义汇率，系统首先使用该汇率数据）
+	UpdateTime string  `json:"update_time"` // 更新时间
 }
 
 type RatesQueryParams struct {
